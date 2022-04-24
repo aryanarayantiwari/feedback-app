c2a7f7caaf0e955b9e03d4d83f9338e37fed71d3
Arya <aryanarayantiwari@gmail.com>
2022-04-24 18:05:42 +0530

    add feedback-app

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = [
+    {
+      id: "73621566-0fd7-41ea-91f1-944a608c9d7a",
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: "72ecd329-9c7e-4cbd-acc9-72743ed598af",
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: "8ca86d89-189e-4923-b144-60321ac8b82f",
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ]
+  
+  export default FeedbackData
+  
